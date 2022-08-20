23e00884072552ac8e07ec967484251ae9c4bbad
“jms” <jstapleton536@gmail.com>
2022-08-20 11:37:40 -0400

    implemented timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
+var timerElement = document.querySelector(".timer");
+var timerCounter;
+var timer;
+
 var startButton = document.querySelector("#start-quiz");
-startButton.addEventListener("click", function() {
+
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCounter--;
+      console.log(timerCounter);
+      timerElement.textContent = timerCounter;
+      if (timerCounter === 0) {
+        // Clears interval
+        clearInterval(timer);
+        console.log("loser");
+      }
+    }, 1000);
+  }
+
+  startButton.addEventListener("click", function() {
     console.log(startButton.innerHTML);
-    
-
-
-
-
+    timerCounter = 10;
+    startTimer();
 });
