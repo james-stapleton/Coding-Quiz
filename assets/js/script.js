45df95627e3f9d3b1b141dd093efe1190a1b951f
“jms” <jstapleton536@gmail.com>
2022-08-23 19:15:10 -0400

    Created array of objects with questions and answers and began implementing logic for quiz start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 var timerElement = document.querySelector(".timer");
+var timerText = document.querySelector("#timer-text");
 var timerCounter;
 var timer;
+var questionElement = document.querySelector("#question");
+var questionNumber = 0; //index into questionArray 
+var answerElement = document.querySelector("#answer-list");
+
+var questionArray = [ 
+    {question: "What is JavaScript?",
+    choices: ["A website", "A scripting language", "A browser", "A computer"],
+    answer: 2
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+]
 
 var startButton = document.querySelector("#start-quiz");
 
 function startTimer() {
     // Sets timer
+    timerText.textContent = " seconds remaining";
     timer = setInterval(function() {
       timerCounter--;
       console.log(timerCounter);
@@ -21,5 +38,13 @@ function startTimer() {
   startButton.addEventListener("click", function() {
     console.log(startButton.innerHTML);
     timerCounter = 10;
+    quiz();
     startTimer();
-});
+    });
+
+function quiz() {
+    questionElement.textContent = questionArray[questionNumber].question;
+    var li = document.createElement("li");
+    li.textContent = "TEST PLEASE WORK";
+    answerElement.appendChild(li);
+}
